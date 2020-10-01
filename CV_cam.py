8872f5568b80487a7c0548a5c7893a6e0ddda4a0
Gautamjhawar <jhawargautam@mgail.com>
2020-10-01 12:41:22 +0530

    Added complete crosshair (Green) to the center of the frame

--- CV_cam.py
+++ CV_cam.py
@@ -5,10 +5,12 @@ cap=cv2.VideoCapture(0)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    (w,h,c) = frame.shape
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.line(frame,200,400,'green',10)
+    gray = cv2.line(frame,(300,240),(340,240),(0,255,0),2)
+    gray = cv2.line(frame,(320,220),(320,260),(0,255,0),2)
     
     # Display the resulting frame
     cv2.imshow('frame',gray)
